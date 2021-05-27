a39bdac96517a8112ba079dfa266a164d4e4d371
Jose Rodriguez <isc.joserodriguez@gmail.com>
2021-05-26 19:33:04 -0600

    Ejemplo 2

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
+const divisas = require('./divisas.json');
+
 const express = require('express');
+
 const app = express();
 
-app.use(express.text());
+
 app.use(express.json());
 
 app.get('/', function (request, response) {
-    console.log(request.get('Mi-encabezado-personalizado'));
     response.end('Hello BEDU');
 });
 
-app.get('/user-agent', function (request, response) {
-    const userAgent = request.get('User-Agent');
-    const who = request.get('Host');
-    response.end(`Consultado desde: ${userAgent} / ${who}`);
+app.get('/divisas', function (request, response) {
+    const catalogo = divisas.map(divisa => divisa.moneda);
+    response.json(catalogo);
+});
+console.log(divisas)
+
+app.get('/divisas/:moneda', function (request, response) {
+    const { moneda } = request.params;
+    const valores = divisas.filter(divisa => divisa.moneda == moneda);
+    if (valores.length) {
+        response.json(valores[0].tipoCambio);
+    } else {
+        response.send('No existe esa moneda');
+    }
 });
 
-app.post('/body', function (request, response) {
-    console.log(request.body)
-    response.end();
-});
+app.get('/divisas/:monedaA/:monedaB', function (request, response) {
+    const { monedaA, monedaB } = request.params;
+    const valores = divisas.filter(divisa => divisa.moneda == monedaA);
 
+    if (valores.length) {
+        const cambio = valores[0].tipoCambio.filter(divisa => divisa.moneda == monedaB);
+        if (cambio.length) {
+            response.send(`1 ${monedaA} vale ${cambio[0].valor}`);
+        } else {
+            response.send(`No existe el cambio de ${monedaA} a ${monedaB}`);
+        }
+    } else {
+        response.send(`No existe el cambio de ${monedaA}`);
+    }
+});
 
 
 app.listen(8080, function () {
